dffa8055c4c1b4f861b69f166cc9b18a4939ad6b
Macauley Drake <macauleyann@gmail.com>
2018-09-26 19:44:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
-  delete driver[key]
+  delete driver["key"]
 }
